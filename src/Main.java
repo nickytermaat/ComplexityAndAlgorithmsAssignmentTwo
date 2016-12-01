07bbfe2347c876f7ec833acf44babfc6d712e359
Nicky <nickytermaat24051996@live.nl>
2016-12-01 13:53:11 +0100

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,17 @@
+import java.io.PrintWriter;
+
 /**
  * Created by Nicky on 01/12/2016.
  */
 public class Main {
 
     //Hello
+    public static void main(String[] args) {
+
+    }
+
+    private String toCSV() {
+//        PrintWriter pw = new PrintWriter(File filename)
+    }
 
 }
