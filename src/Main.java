03990d0a721983ee2eb39ed6f215b39be277638f
Nicky <nickytermaat24051996@live.nl>
2016-12-07 21:58:27 +0100

    Update buildHeap, Add percolateDown

--- src/Main.java
+++ src/Main.java
@@ -15,38 +15,6 @@ public class Main {
         main.readFromFile(file);
     }
 
-    // parent at position i
-    // left child at position 2i
-    // right child at position 2i + 1
-
-    // child at position i
-    // parent at position (i/2)
-
-    // (minHeap) Parent's key <= children's key
-    // (maxHeap) Parent's key >= children's key
-
-    // The tree is filled from top to bottom, from left to right
-    // All levels are completely filled, except possible the lowest level
-    // The lowest level is filled from the left (no holes)
-
-    // Percolate down:  delete the root from the heap (max element in maxHeap, min element in minHeap)
-    //                  1. replace the root of the heap with the last element on the lowest level
-    //                  2. compare the new root with its children; if they are in the correct order, stop
-    //                  3. if not, swap the element with one of its children and return to the previous step
-
-    // Percolate up:    1. add the element to the bottom level of the heap
-    //                  2. compare the added element with its parent; if they are in the correct order, stop
-    //                  3. if not, swap the element with its parent and return to the previous step.
-
-    // insert(element) - insert element into heap - O(log N)
-    // findMin - returns smallest element of a heap - O(1)
-    // deleteMin - removes smallest element from a heap - O(log N)
-
-    // In case of N alterning insertions and deletions: apply insert and deleteMin (of oder O(N log N)
-    // In case of N insertions before deletions: apply insert to each element (of order O(N log N)
-    // At the time of deletion the heap must be correct, not necessarily after each insertion
-
-    // buildHeap - percolateDown repeatedly from the rear (starting at the last parent)
 
 
 
--- src/RSHeap.java
+++ src/RSHeap.java
@@ -25,6 +25,41 @@ public class RSHeap {
         this.runs = 0;
     }
 
+
+
+    // parent at position i
+    // left child at position 2i
+    // right child at position 2i + 1
+
+    // child at position i
+    // parent at position (i/2)
+
+    // (minHeap) Parent's key <= children's key
+    // (maxHeap) Parent's key >= children's key
+
+    // The tree is filled from top to bottom, from left to right
+    // All levels are completely filled, except possible the lowest level
+    // The lowest level is filled from the left (no holes)
+
+    // Percolate down:  delete the root from the heap (max element in maxHeap, min element in minHeap)
+    //                  1. replace the root of the heap with the last element on the lowest level
+    //                  2. compare the new root with its children; if they are in the correct order, stop
+    //                  3. if not, swap the element with one of its children and return to the previous step
+
+    // Percolate up:    1. add the element to the bottom level of the heap
+    //                  2. compare the added element with its parent; if they are in the correct order, stop
+    //                  3. if not, swap the element with its parent and return to the previous step.
+
+    // insert(element) - insert element into heap - O(log N)
+    // findMin - returns smallest element of a heap - O(1)
+    // deleteMin - removes smallest element from a heap - O(log N)
+
+    // In case of N alterning insertions and deletions: apply insert and deleteMin (of oder O(N log N)
+    // In case of N insertions before deletions: apply insert to each element (of order O(N log N)
+    // At the time of deletion the heap must be correct, not necessarily after each insertion
+
+    // buildHeap - percolateDown repeatedly from the rear (starting at the last parent)
+
     public int[] run() throws FileNotFoundException {
         //Run through the unsorted array and insert the stuff into the heapArray where it's needed
 
@@ -85,6 +120,50 @@ public class RSHeap {
     }
 
     private void buildHeap(int[] unsorted) {
-
+        for (int i = maxHeapSize/2; i < 0; i--) {
+            percolateDown(i);
+        }
     }
+
+    /**
+     * Percolate down from a given node in the heap
+     * @param startNode the index of the node at which the percolate starts
+     */
+    private void percolateDown(int startNode) {
+        int child;
+        int temp = heapArray[startNode];
+        int hole;
+
+        // The hole is the startNode, the hole*2 must be smaller or equal to the heapsize, the hole becomes the child
+        for(hole = startNode; hole * 2 <= maxHeapSize; hole = child) {
+            child = hole * 2;
+            boolean childCompare;
+
+            // If the left child is smaller than the right child
+            if (heapArray[child] <= heapArray[child+1]){
+                childCompare = true; // the left child should become the parent node
+            } else { // Else left child bigger than right child
+                childCompare = false; // the right child should become the parent node
+            }
+
+            // If the childnodeposition is smaller than the max heapsize and
+            // the left child is bigger than the right child
+            // increase the child-index so it becomes the right child
+            if (child != maxHeapSize && !childCompare) {
+                child++;
+            }
+
+            // if the element on the heapindex child is smaller than the temp value,
+            // the value on the startnode index becomes the value found at childindex
+            if (!(heapArray[child] < temp)) {
+                heapArray[startNode] = heapArray[child];
+            } else {
+                break;
+            }
+
+        }
+
+        heapArray[hole] = temp;
+    }
+
 }
