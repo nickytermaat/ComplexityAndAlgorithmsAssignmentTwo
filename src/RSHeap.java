199caa618c5aeeb71d79dbf7aea090f96cab4cce
Kris <krisscholte@hotmail.com>
2016-12-07 20:16:04 +0100

    Setup for Replacement Sorting

--- src/RSHeap.java
+++ src/RSHeap.java
@@ -9,14 +9,26 @@ public class RSHeap {
     public RSHeap(int heapSize, int[] unsorted) {
         this.heapSize = heapSize;
         this.unsorted = unsorted;
-        this.heapArray = new int[heapSize];
     }
 
     public int[] run(){
         //Run through the unsorted array and insert the stuff into the heapArray where it's needed
 
-
+        // Build the heap
+        heapArray = new int[heapSize];
+        int amount = 0;
+        for (int i = 0; i < unsorted.length; i++) {
+            if(i <= heapSize){                                      //There's still room in the heap
+                heapArray[i] = unsorted[i];
+            } else {                                                //The heap is full. Time to sort it!
+                buildHeap(unsorted);
+                
+            }
+        }
         return heapArray;
+    }
+
+    private void buildHeap(int[] unsorted) {
     }
 
 
