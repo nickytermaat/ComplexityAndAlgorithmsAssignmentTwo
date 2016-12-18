3c186c24431e71685a9f0e09144c292528eab45b
Kris <krisscholte@hotmail.com>
2016-12-18 11:13:01 +0100

    Heap

--- src/RSHeap.java
+++ src/RSHeap.java
@@ -26,25 +26,6 @@ public class RSHeap {
         this.pw = new PrintWriter(out);
         this.runs = 0;
     }
-    // parent at position i
-    // left child at position 2i
-    // right child at position 2i + 1
-
-    // child at position i
-    // parent at position (i/2)
-
-    // (minHeap) Parent's key <= children's key
-    // (maxHeap) Parent's key >= children's key
-
-    // The tree is filled from top to bottom, from left to right
-    // All levels are completely filled, except possible the lowest level
-    // The lowest level is filled from the left (no holes)
-
-    // Percolate down:  delete the root from the heap (max element in maxHeap, min element in minHeap)
-    //                  1. replace the root of the heap with the last element on the lowest level
-    //                  2. compare the new root with its children; if they are in the correct order, stop
-    //                  3. if not, swap the element with one of its children and return to the previous step
-
     // Percolate up:    1. add the element to the bottom level of the heap
     //                  2. compare the added element with its parent; if they are in the correct order, stop
     //                  3. if not, swap the element with its parent and return to the previous step.
@@ -61,60 +42,41 @@ public class RSHeap {
 
     public int[] run() {
         //Run through the unsorted array and insert the stuff into the heapArray where it's needed
-
         // Build the heap
         int i = 0;
         heapArray = new int[maxHeapSize+1];
-        while(i < unsorted.length){                                              //Loop through all unsorted numbers
-            if(amountOfNumbersInside < maxHeapSize && i != (unsorted.length)){     //There's still room in the heap
-                // && we're not at the last element
-                insertInHeap(unsorted[i]);
+        while(i < unsorted.length){                                             //Loop through all unsorted numbers
+            if(amountOfNumbersInside < maxHeapSize && i != unsorted.length-1){                            //Add number to heap
+                heapArray[amountOfNumbersInside + 1] = unsorted[i];
+                amountOfNumbersInside++;
                 i++;
-            } else {
-                //Now... We're at a full heap and need to remove one.
-                int minVal = removeMinFromHeap();
-                System.out.println("Writing minval " + minVal);
-                if(unsorted[i] > peekAtSmallest()){
-                    //Next incoming value is bigger than our smallest, so all is good
-                    insertInHeap(unsorted[i]);
-                    i++;
-                } else {
-                    maxHeapSize--;
-                    deadspace++;
-                    heapArray[maxHeapSize + 1] = unsorted[i]; //Insert into deadspace
-                    i++;
-                }
-            }
-            if(maxHeapSize == 0){
-                //The deadspace is now full. Time to sort it as a heap.
-                //Build a new heap in the deadSpace
-                maxHeapSize = deadspace;
-                deadspace = 0;
+            } else {                                                            //Heap is full, now build it
                 buildHeap();
+                //TODO: Delete smallest element
+                //TODO: Check if new element (unsorted[i]) should be placed in the heap or in the deadspace
+                //TODO: Put element where it belongs
             }
         }
-        while(0 < amountOfNumbersInside){
-            System.out.println("Removed" + removeMinFromHeap());
+
+        while(maxHeapSize > 0){
+            System.out.println("Deleting " + removeMinFromHeap());
+            maxHeapSize--;
         }
-        System.out.println("Done");
-        pw.flush();
+        System.out.println("Done!");
         return heapArray;
     }
 
-    private int peekAtSmallest(){
-        return heapArray[1];
-    }
     private void buildHeap() {
-        for (int i = maxHeapSize/2; i > 0; i--) {
+        for (int i = maxHeapSize /2; i > 0; i--) {
             percolateDown(i);
         }
-        System.out.println("Finished building");
     }
 
-    public int removeMinFromHeap(){
+    private int removeMinFromHeap(){
         int value = heapArray[1];
+        heapArray[1] = heapArray[amountOfNumbersInside];
+        amountOfNumbersInside-- ;
         percolateDown(1);
-        amountOfNumbersInside--;
         return value;
     }
 
@@ -128,7 +90,7 @@ public class RSHeap {
         //Find smallest of 2 children
         //Left child = index * 2
         //Right child = (index * 2 ) +1
-        if((index * 2) + 1 < amountOfNumbersInside){
+        if((index * 2) + 1 <= amountOfNumbersInside){
             //RightChild exists. I'm checking with left.
             int leftChild = heapArray[(index * 2)];
             int rightChild = heapArray[(index * 2) + 1];
@@ -141,17 +103,12 @@ public class RSHeap {
                 heapArray[index] = rightChild;
                 percolateDown(index * 2 + 1);
             }
-        } else if((index * 2) == amountOfNumbersInside) {
+        } else if((index * 2) == amountOfNumbersInside && heapArray[index * 2] < heapArray[index]) {
             //The LeftChild is the last child in the tree, swap
             int leftChild = heapArray[(index * 2)];
             heapArray[index * 2] = heapArray[index];
             heapArray[index] = leftChild;
-        } else {
-            //I'm at the last node. Remove
-            //TODO: This should NOT be here. Percolating down should stop now.
-//            heapArray[index] = removeMaxFromHeap();
         }
-
     }
 
     private void insertInHeap(int number){
